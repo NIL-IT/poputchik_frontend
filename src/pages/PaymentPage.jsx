6336107ccf403ab545145e6bc838ec5b7072cf00
Anton <79142916+skyfxllen@users.noreply.github.com>
2025-04-09 11:56:30 +0700

    cors test

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 import BackButton from "../components/NavigationButton/components/BackButton/BackButton";
 import Button from "../components/Button/Button";
 import { useUserStore } from "../state/UserStore";
+import { getStatus, payment } from "../api/payment";
 
 export default function PaymentPage() {
   const terminalkey = "1743154896913DEMO";
+  const password = "5agKh^xldCr9okSw";
   const [summ, setSumm] = useState("");
   const { currentUser } = useUserStore();
   const navigate = useNavigate();
@@ -13,7 +15,7 @@ export default function PaymentPage() {
 
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://securepay.tinkoff.ru/html/payForm/js/tinkoff.js";
+    script.src = "https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js";
     script.async = true;
     document.body.appendChild(script);
     return () => {
@@ -21,15 +23,34 @@ export default function PaymentPage() {
     };
   }, []);
 
-  const handleSubmit = (e) => {
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   getStatus();
+  //   if (window.pay) {
+  //     window.pay(event.target);
+  //   } else {
+  //     console.error("Скрипт оплаты не загружен");
+  //   }
+  // };
+  async function pay(e) {
     e.preventDefault();
-    if (window.pay) {
-      window.pay(formRef.current);
-    } else {
-      console.error("Функция pay не загружена");
+    const form = {
+      TerminalKey: terminalkey,
+      paymentInfo: {
+        PaymentData: {
+          TerminalKey: terminalkey,
+          Amount: summ * 100,
+        },
+      },
+    };
+    console.log("first");
+    try {
+      await getStatus();
+      await payment(form);
+    } catch (error) {
+      console.log(error.message || "Неизвестная ошибка");
     }
-  };
-
+  }
   return (
     <div className='py-10 relative flex flex-col items-center justify-between w-full min-h-screen'>
       <div className='flex flex-col'>
@@ -37,81 +58,72 @@ export default function PaymentPage() {
         <h3 className='font-bold text-[20px] leading-5 pb-10'>Вывод средств</h3>
         <form
           ref={formRef}
-          className='flex flex-col gap-5 w-full justify-center items-center'
-          name='TinkoffPayForm'
-          onSubmit={handleSubmit}>
+          className='payform-tbank flex flex-col gap-5 w-full justify-center items-center'
+          name='payform-tbank'
+          onSubmit={pay}>
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             name='terminalkey'
             value={terminalkey}
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             name='frame'
-            value='true'
+            value='false'
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             name='language'
             value='ru'
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='text'
-            placeholder='Сумма вывода'
-            value={summ}
-            onChange={(e) => setSumm(e.target.value)}
+            placeholder='Сумма заказа'
             name='amount'
             required
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             placeholder='Номер заказа'
             name='order'
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
-            placeholder='Описание заказа'
+            placeholder='Описание заказа input'
             name='description'
           />
           <input
-            className='tinkoffPayRow input'
-            type='text'
-            placeholder='ФИО клиента'
+            className='payform-tbank-row input'
+            type='hidden'
+            placeholder='ФИО плательщика input'
             name='name'
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             placeholder='E-mail'
-            value={currentUser.email}
             name='email'
           />
           <input
-            className='tinkoffPayRow input'
+            className='payform-tbank-row input'
             type='hidden'
             placeholder='Контактный телефон'
-            value={currentUser.phone_number}
             name='phone'
           />
-          {/* <input
-            className='tinkoffPayRow'
-            type='hidden'
-            name='receipt'
-            value=''
-          /> */}
-          <input
-            className='tinkoffPayRow input'
+
+          <Button
             type='submit'
-            value='Оплатить'
-          />
+            className='payform-tbank-btn'
+            size='large'>
+            Оплатить
+          </Button>
         </form>
-        <Button size='large'>Вывести</Button>
       </div>
     </div>
   );
